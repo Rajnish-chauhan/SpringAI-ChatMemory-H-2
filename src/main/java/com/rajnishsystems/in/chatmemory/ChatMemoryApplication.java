7e2bbcd1627a43897e65bac12e82cbaf3c48e678
Rajnish-chauhan <crajnish425@gmail.com>
2026-09-23 22:46:44 +0530

    store chat memory into H-2 Database

---
+++ src/main/java/com/rajnishsystems/in/chatmemory/ChatMemoryApplication.java
@@ -0,0 +1,13 @@
+package com.rajnishsystems.in.chatmemory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatMemoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatMemoryApplication.class, args);
+    }
+
+}
